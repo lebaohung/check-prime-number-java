25c617ad5702913cb046303a2b43405172c73c90
Hung <lebaohung.1996@gmail.com>
2020-03-22 00:50:37 +0700

    update

---
+++ src/com/company/Main.java
@@ -0,0 +1,37 @@
+package com.company;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("CHECK PRIME NUMBER.");
+        System.out.print("Enter number: ");
+        Scanner scanner = new Scanner(System.in);
+        String yearStr = scanner.nextLine();
+        if(yearStr.matches("^(-?)[0-9]+$") ) {
+            int year = Integer.parseInt(yearStr);
+            if (year <= 1 || (year > 2 && year % 2 == 0)){
+                System.out.println(year + " is not a prime number");
+            } else {
+                switch (year){
+                    case 2:
+                    case 3:
+                        System.out.println(year + " is a prime number");
+                        break;
+                    default:
+                        boolean status = true;
+                        for (int i = 3; i < Math.sqrt(year); i+=2) {
+                            if (year % i == 0) {
+                                System.out.println(year + " is not a prime number");
+                                status = false;
+                                break;
+                            }
+                        }
+                        if (status) System.out.println(year + " is a prime number");
+                }
+            }
+        } else {
+            System.out.println("\nInvalid input!");
+            main(args);
+        }
+    }
+}
+
